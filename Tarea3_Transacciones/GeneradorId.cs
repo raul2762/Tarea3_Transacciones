92e670f88add385aaad4d7d86e5b4eb8673c1f24
Raul Rodriguez <raul2762@hotmail.com>
2020-06-02 23:07:00 -0400

    add class transaction

---
+++ Tarea3_Transacciones/GeneradorId.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea3_Transacciones
+{
+	public class GeneradorId
+	{
+		private static GeneradorId _instancia = null;
+		List<int> idGenerados = new List<int>();
+		public static GeneradorId Instancia
+		{
+			get
+			{
+				if (_instancia == null)
+				{
+					_instancia = new GeneradorId();
+				}
+				return _instancia;
+			}
+		}
+		public int GetIdRnd()
+		{
+			Random random = new Random();
+			bool veryfied = true;
+			int id = 0;
+			while (veryfied)
+			{
+				id = random.Next(1000);
+				veryfied = idGenerados.Contains(id);
+			}
+
+			return id;
+		}
+	}
+}
---
+++ Tarea3_Transacciones/Transaccion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea3_Transacciones
+{
+	enum TipoTransaccion
+	{
+		Aprobada = 1,
+		Rechazada,
+		Cancelada,
+		Eliminada
+	}
+	public class Transaccion
+	{
+		public int No_trans { get; set; }
+		public string Nombre { get; set; }
+		public double Monto { get; set; }
+	}
+}
